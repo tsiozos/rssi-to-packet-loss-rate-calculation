7bf5b4dba72023b130a2bd4b4e9d90625a18b5e8
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-10-26 13:10:08 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,40 @@
+radio.set_transmit_power(7)
+radio.set_group(99)
+packetsRCVD = 0
+sumRSSI = 0
+
+def on_received_value(name, value):
+    global packetsRCVD, sumRSSI
+    if name=="RST":
+        packetsRCVD = 0
+        sumRSSI = 0
+    elif name=="SYNC":
+        rssi = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
+        packetsRCVD = packetsRCVD + 1
+        sumRSSI = sumRSSI+rssi
+        serial.write_value("SUCCESS %", packetsRCVD/value)
+    elif name=="ENDSYNC":
+        serial.write_line("")
+        serial.write_value("SUCCESS RATE", packetsRCVD/1000)
+        serial.write_value("AVERAGE RSSI", sumRSSI/packetsRCVD)
+
+radio.on_received_value(on_received_value)
+
+def on_button_pressed_ab():
+    for i in range(10):
+        basic.show_number(9-i)
+    basic.pause(1000)
+    basic.clear_screen()
+    for i in range(100):
+        radio.send_value("RST", 0)
+    basic.pause(50)
+
+    for i in range(1000):
+        radio.send_value("SYNC", i)
+    led.toggle(2,2)
+    basic.pause(100)
+
+    for i in range(100):
+        radio.send_value("ENDSYNC", 0)
+    basic.pause(50)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
---
+++ main.ts
@@ -0,0 +1,43 @@
+radio.setTransmitPower(7)
+radio.setGroup(99)
+let packetsRCVD = 0
+let sumRSSI = 0
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let rssi: number;
+    
+    if (name == "RST") {
+        packetsRCVD = 0
+        sumRSSI = 0
+    } else if (name == "SYNC") {
+        rssi = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+        packetsRCVD = packetsRCVD + 1
+        sumRSSI = sumRSSI + rssi
+        serial.writeValue("SUCCESS %", packetsRCVD / value)
+    } else if (name == "ENDSYNC") {
+        serial.writeLine("")
+        serial.writeValue("SUCCESS RATE", packetsRCVD / 1000)
+        serial.writeValue("AVERAGE RSSI", sumRSSI / packetsRCVD)
+    }
+    
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let i: number;
+    for (i = 0; i < 10; i++) {
+        basic.showNumber(9 - i)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    for (i = 0; i < 100; i++) {
+        radio.sendValue("RST", 0)
+    }
+    basic.pause(50)
+    for (i = 0; i < 1000; i++) {
+        radio.sendValue("SYNC", i)
+    }
+    led.toggle(2, 2)
+    basic.pause(100)
+    for (i = 0; i < 100; i++) {
+        radio.sendValue("ENDSYNC", 0)
+    }
+    basic.pause(50)
+})
